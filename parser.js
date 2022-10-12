4bae4c796b921d90703d90f44f6f79a9176916ce
tureptor <mansooramiri@protonmail.com>
2022-10-12 17:32:54 +0100

    Separate classes and practicals by day+time Adds support for classes/practicals which have multiple groups

--- parser.js
+++ parser.js
@@ -29,20 +29,32 @@ function parseICS(data) {
 
 // returns an JSON object of all VEVENTS categorised by course name
 function getEvents(lines, starts, ends) {
+    const weekday = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
     allEvents = {};
     // iterate through startIndices/endIndices and parse each VEVENT
     for (let i = 0; i < starts.length; i++) {
         curEvent = {};
         // for each line in the event
         for (const line of lines.slice(starts[i],ends[i]+1)) {
-            m = line.match(/(.*):(.*)/); // split x:y into x,y
+            const m = line.match(/(.*):(.*)/); // split x:y into x,y
             curEvent[m[1]] = m[2];
         }
         // append type (lect,prac,class) so we can distinguish within course
         curEvent["SUMMARY"] += " " + curEvent["CATEGORIES"];
-        // make array to hold this course if it doesn't exist
-        allEvents[curEvent["SUMMARY"]] = allEvents[curEvent["SUMMARY"]] || [];
-        allEvents[curEvent["SUMMARY"]].push(curEvent);
+        
+        //if practical or class, then group by its day/time
+        if (curEvent["CATEGORIES"] != "Lecture") {
+            const matchdatetime = /(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})/
+            const m = curEvent["DTSTART;TZID=Europe/London"].match(matchdatetime);
+            const daytime =  " " + weekday[new Date(m.slice(1,4).join("-")).getDay()] + " " + m.slice(4,6).join(":");
+            
+            allEvents[curEvent["SUMMARY"]+daytime] = allEvents[curEvent["SUMMARY"]+daytime ] || [];
+            allEvents[curEvent["SUMMARY"]+daytime].push(curEvent)
+        } else {
+            // is lecture so no need to group by start time
+            allEvents[curEvent["SUMMARY"]] = allEvents[curEvent["SUMMARY"]] || [];
+            allEvents[curEvent["SUMMARY"]].push(curEvent);
+        }
     }
     return allEvents;
 }
